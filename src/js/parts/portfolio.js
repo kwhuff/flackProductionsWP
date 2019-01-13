891d37d6177eb25f1de1b68789fefee1114f6fa5
Kyle Huff <khuff@rocketbuild.com>
2019-01-13 15:32:31 -0500

    js adjustment

--- src/js/parts/portfolio.js
+++ src/js/parts/portfolio.js
@@ -7,11 +7,13 @@ src=${e.toElement.dataset.attribute} frameborder="0" allow="accelerometer; autop
 gyroscope; picture-in-picture" allowfullscreen></iframe>`;
         $('.all-videos-wrapper').css('display', 'none');
         $('#video-player-wrapper').append(toAppend).css('display', 'block');
+        $('p').css('display', 'none');
         $('#show-videos').on('click', function() {
             $('#show-videos').remove();
             $('#current-video').remove();
             $('#video-player-wrapper').css('display', 'none');
             $('.all-videos-wrapper').css('display', 'block');
+            $('p').css('display', 'block');
         });
     });
 }
